bf7ef21b62da1fea2852d26e6e401d4e60449169
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-12 10:16:51 +0530

    Pooja Added: Welcome To NUnit Programs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day6_ProgramForNUnitTesting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UC1_VendingMachine.countCurrency();
+            //UC2_DaysOfWeek.days();
+            //UC3_DegreeConversion.Degree();
+            //UC4_MonthlyPayment.pay();
+            // UC5_NewtonsMethod.square();
+            // UC6_DecimalToBinary.tobinary();
+
+
+
+
+        }
+    }
+}
