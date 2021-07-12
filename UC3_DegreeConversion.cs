8c0f7e58daff736dd98cf94c319a6b2c1b49cb7c
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-12 10:23:13 +0530

    Pooja Added: Degree Conversion Program

---
+++ UC3_DegreeConversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_ProgramForNUnitTesting
+{
+	class UC3_DegreeConversion
+	{
+		public static void Degree()
+        {
+			int Celcius;
+			Console.WriteLine("Enter the Temperature in Celcius");
+			Celcius = int.Parse(Console.ReadLine());
+			//Converting Celsius to Fahrenheit
+			int Fahrenheit = (Celcius * 9 / 5) + 32;
+			Console.WriteLine("After Converting: " + Fahrenheit);
+
+			Console.WriteLine("Enter the Farhenheit");
+            Celcius = int.Parse(Console.ReadLine());
+            Celcius = (Fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("After Conveting: " + Celcius);
+
+		}
+
+	}
+}
